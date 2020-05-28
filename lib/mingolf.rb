270f5292e1cce0cee1ceabab153991ddab190d2f
Johan Andersson <johan.rejeep@gmail.com>
2020-05-28 09:38:10 +0200

    Add bin/mingolf
    Currently only checking free slots at a list of hardcoded courses.

---
+++ lib/mingolf.rb
@@ -0,0 +1,134 @@
+# coding: utf-8
+
+require 'date'
+require 'json'
+require 'optparse'
+require 'time'
+require 'typhoeus'
+
+class Mingolf
+  USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'.freeze
+
+  COURSES = [
+    %w[2ef1890b-1867-4ad5-9589-a12896888076 525b12ef-60a5-11d8-bc62-b74e72f35744], # Kungsbacka
+    %w[23aede8b-f55c-4706-8289-7b94d3212704 d98962ab-f4d5-4355-abe2-f3a1146ca9c3], # Vallda
+    %w[a9d7060f-0051-40fc-a021-98b885a55300 f6845066-ac12-4971-b7c9-327629d06b50], # St. Jörgen
+  ].freeze
+
+  def initialize(argv, http: nil, io: nil, courses: nil, attempts: nil, sleeper: nil, executer: nil)
+    @options = parse_options(argv)
+    @date = @options.fetch(:date)
+    @from = @options.fetch(:from)
+    @to = @options.fetch(:to)
+    @slots = @options.fetch(:slots)
+    @http = http || Typhoeus
+    @io = io || STDOUT
+    @courses = courses || COURSES
+    @attempts = attempts || 100_000
+    @sleeper = sleeper || Kernel
+    @executer = executer || Kernel
+  end
+
+  def run
+    login
+    @attempts.times do
+      free_slots = []
+      @courses.each do |club_id, course_id|
+        url = format(
+          'https://mingolf.golf.se/handlers/booking/GetTeeTimesFullDay/%s/%s/%sT090000/1',
+          course_id,
+          club_id,
+          @date.strftime('%Y%m%d'),
+        )
+        @io.puts(url)
+        response = @http.get(
+          url,
+          cookiefile: cookiefile,
+          cookiejar: cookiefile,
+          headers: {
+            'Accept' => '*/*',
+            'User-Agent' => USER_AGENT,
+          },
+        )
+        response_body = JSON.parse(response.body)
+        response_body.fetch('Slots').each do |slot|
+          slot_time = Time.strptime(slot.fetch('SlotTime'), '%Y%m%dT%H%M%S')
+          if slot_time >= @from && slot_time <= @to
+            if participants = response_body.fetch('Participants').dig(slot.fetch('SlotID'))
+              if slot.fetch('MaximumNumberOfSlotBookingsPerSlot') - participants.size >= @slots
+                free_slots << slot
+              end
+            end
+          end
+        end
+      end
+      if free_slots.empty?
+        @io.puts('No free slots')
+      else
+        @io.puts('Free slots found')
+        free_slots.each do |free_slot|
+          @io.puts(format('%s at %s', Time.strptime(free_slot.fetch('SlotTime'), '%Y%m%dT%H%M%S'), free_slot.fetch('OrganizationalunitName')))
+        end
+        @io.puts('')
+        @executer.system %|say "#{free_slots.size} free slots found"|
+      end
+      @sleeper.sleep(60)
+    end
+  end
+
+  private
+
+  def login
+    @http.post(
+      'https://mingolf.golf.se/handlers/login',
+      body: {
+        golfID: golf_id,
+        password: password,
+        remember: false,
+      },
+      cookiefile: cookiefile,
+      cookiejar: cookiefile,
+      headers: {
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/x-www-form-urlencoded; charset=UTF-8',
+        'User-Agent' => USER_AGENT,
+      },
+    )
+  end
+
+  def parse_options(argv)
+    options = {}
+    optparse = OptionParser.new do |opts|
+      opts.on('--date DATE') do |date|
+        options[:date] = Date.parse(date)
+      end
+      opts.on('--from FROM') do |from|
+        options[:from] = Time.parse("#{options.fetch(:date)} #{from}")
+      end
+      opts.on('--to TO') do |to|
+        options[:to] = Time.parse("#{options.fetch(:date)} #{to}")
+      end
+      opts.on('--slots SLOTS', Integer) do |slots|
+        options[:slots] = slots
+      end
+    end
+    optparse.parse!(argv)
+    options
+  end
+
+  def cookiefile
+    File.join(__dir__, '..', '.cookie')
+  end
+
+  def golf_id
+    auth[0]
+  end
+
+  def password
+    auth[1]
+  end
+
+  def auth
+    @auth ||= File.read(File.join(__dir__, '..', '.auth')).lines.map(&:strip)
+  end
+end
---
+++ spec/mingolf_spec.rb
@@ -0,0 +1,128 @@
+# coding: utf-8
+require 'mingolf'
+
+describe Mingolf do
+  let :argv do
+    %w[--date 2020-05-30 --from 10:00 --to 15:00 --slots 2]
+  end
+
+  let :http do
+    double(:http)
+  end
+
+  let :io do
+    double(:io, puts: nil)
+  end
+
+  let :courses do
+    [
+      %w[CLUB_ID COURSE_ID],
+    ]
+  end
+
+  let :sleeper do
+    double(:sleeper, sleep: nil)
+  end
+
+  let :executer do
+    double(:executer, system: nil)
+  end
+
+  let :slot_time do
+    '20200530T113000'
+  end
+
+  let :maximum_number_of_slot_bookings_per_slot do
+    4
+  end
+
+  let :tee_times_full_day_response_body_participants do
+    {
+      'SLOT_ID' => [
+        {'ExactHcp' => '1,2'},
+        {'ExactHcp' => '3,4'},
+      ],
+    }
+  end
+
+  let :tee_times_full_day_response_body do
+    {
+      'Slots' => [
+        {
+          'MaximumNumberOfSlotBookingsPerSlot' => maximum_number_of_slot_bookings_per_slot,
+          'SlotID' => 'SLOT_ID',
+          'SlotTime' => slot_time,
+          'OrganizationalunitName' => 'OrganizationalunitName',
+        },
+      ],
+      'Participants' => tee_times_full_day_response_body_participants,
+    }
+  end
+
+  let :tee_times_full_day_response do
+    double(:tee_times_full_day_response, body: JSON.dump(tee_times_full_day_response_body))
+  end
+
+  let :tee_times_full_day_url do
+    'https://mingolf.golf.se/handlers/booking/GetTeeTimesFullDay/COURSE_ID/CLUB_ID/20200530T090000/1'
+  end
+
+  subject :mingolf do
+    described_class.new(
+      argv,
+      http: http,
+      io: io,
+      courses: courses,
+      attempts: 1,
+      sleeper: sleeper,
+      executer: executer,
+    )
+  end
+
+  before do
+    allow(http).to receive(:post).with('https://mingolf.golf.se/handlers/login', anything)
+    allow(http).to receive(:get).with(tee_times_full_day_url, anything).and_return(tee_times_full_day_response)
+  end
+
+  it 'prints free slot' do
+    mingolf.run
+    expect(io).to have_received(:puts).with('Free slots found')
+    expect(io).to have_received(:puts).with('2020-05-30 11:30:00 +0200 at OrganizationalunitName')
+  end
+
+  context 'when slot time is outside range' do
+    let :slot_time do
+      '20200530T080000'
+    end
+
+    it 'does not print free slot' do
+      mingolf.run
+      expect(io).to have_received(:puts).with('No free slots')
+    end
+  end
+
+  context 'when number of free slots is not enough' do
+    let :tee_times_full_day_response_body do
+      super().tap do |body|
+        body['Participants']['SLOT_ID'] << {'ExactHcp' => '5,6'}
+        body['Participants']['SLOT_ID'] << {'ExactHcp' => '7,8'}
+      end
+    end
+
+    it 'does not print free slot' do
+      mingolf.run
+      expect(io).to have_received(:puts).with('No free slots')
+    end
+  end
+
+  context 'when slot id is not part of participants' do
+    let :tee_times_full_day_response_body_participants do
+      {}
+    end
+
+    it 'skips slot' do
+      mingolf.run
+      expect(io).to have_received(:puts).with('No free slots')
+    end
+  end
+end
